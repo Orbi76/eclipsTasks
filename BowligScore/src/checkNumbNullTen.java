4317a9f66f4fd8aedd71c31c869037031756af5e
Orbi76bit <oorbangabor@gmail.com>
2019-08-13 14:01:34 +0100

    bekért számokat ellenörzi 0 - 10 között és beirja arraybe.

---
+++ BowligScore/src/checkNumbNullTen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class checkNumbNullTen {
+	
+	private static Scanner beirt;
+	public static int checkNumbNullTen() {
+		beirt = new Scanner(System.in);
+		int beirtSzam;
+		do {
+		//	System.out.println("Please enter a positive number between 0-10");
+			while (!beirt.hasNextInt()) {
+				System.out.println("That's not a number!");
+				beirt.next(); // this is important!
+			}
+			beirtSzam = beirt.nextInt();
+			// } while (number >= 11 || number <= -1); // execute DO till true
+		} while (!(beirtSzam >= 0 && beirtSzam <= 10)); // execute do till true
+	//	System.out.println("Thank you! Got " + beirtSzam);
+		return beirtSzam;
+	}
+}
